07eda22f2ff067a003a52e9bae350ddcc094e65a
aenen <yaroslav.golyuk@gmail.com>
2021-01-05 00:06:01 +0200

    pls work

--- 3dsGallery.DataLayer/Migrations/202101042009446_AddCreationDateColumn.cs
+++ 3dsGallery.DataLayer/Migrations/202101042009446_AddCreationDateColumn.cs
@@ -15,7 +15,7 @@ namespace _3dsGallery.DataLayer.Migrations
             AddColumn("dbo.Picture", "creationDate", c => c.DateTime());
 
             var regex = new Regex("^\\d{1,10}.(?i)(jpg|mpo)$");
-            var pictures = Directory.GetFiles(Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\", "3dsGallery.WebUI", "Picture")))
+            var pictures = Directory.GetFiles(Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Picture")))
                                  .Where(x => regex.IsMatch(Path.GetFileName(x)))
                                  .Select(x => new { ID = Path.GetFileNameWithoutExtension(x), CreationDate = File.GetCreationTime(x).ToString("yyyy'-'MM'-'dd HH:mm:ss.fff") })
                                  .DistinctBy(x => x.ID)
